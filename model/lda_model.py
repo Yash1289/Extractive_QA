b450674aa5239351b03d85a56afb7d5e59173004
yash pandey <knightandday12@gmail.com>
2022-11-18 00:48:24 +0530

    Made minor changed and added more comments

--- model/lda_model.py
+++ model/lda_model.py
@@ -1,21 +1,21 @@
 import gensim
 from gensim.models import LdaModel
 from gensim import models, corpora, similarities
-import time
 
 def train_lda(data):
     """
     This function trains the lda model
-    We setup parameters like number of topics, the chunksize to use in Hoffman method
+    We setup parameters like number of topics, the chunksize to fix the number of document processed at a time
     We also do 4 passes of the data since this is a small dataset, so we want the distributions to stabilize
     """
     num_topics = 8
 
     chunksize = 50
+    #Creating a dictionary representation of the documents
     dictionary = corpora.Dictionary(data['tokenized'])
+    # Bag-of-words representation of the documents.
     corpus = [dictionary.doc2bow(doc) for doc in data['tokenized']]
-    # low alpha means each document is only represented by a small number of topics, and vice versa
-    # low eta means each topic is only represented by a small number of words, and vice versa
+
     lda = LdaModel(corpus=corpus, num_topics=num_topics, id2word=dictionary,
                    alpha=0.9e-2, eta=0.5e-2, chunksize=chunksize, minimum_probability=0.0, passes=4 , random_state = 1)
     return dictionary,corpus,lda
